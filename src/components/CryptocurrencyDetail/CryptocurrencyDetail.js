5c92f20e0e75fe1c7a77da9451deb18701ce294a
Soufianetyk1337 <sz.fanel@gmail.com>
2022-08-28 13:14:20 +0100

    Adding Crypto detail for each coin

--- src/components/CryptocurrencyDetail/CryptocurrencyDetail.js
+++ src/components/CryptocurrencyDetail/CryptocurrencyDetail.js
@@ -1,15 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Chart from "../Chart/Chart";
-import { useParams } from "react-router-dom";
-import Grid from "@mui/material/Grid";
+import { Link, useParams } from "react-router-dom";
 import MoneyOutlinedIcon from "@mui/icons-material/MoneyOutlined";
-import CheckOutlinedIcon from "@mui/icons-material/CheckOutlined";
 import PriorityHighOutlinedIcon from "@mui/icons-material/PriorityHighOutlined";
-import PanToolOutlinedIcon from "@mui/icons-material/PanToolOutlined";
 import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
 import AttachMoneyOutlinedIcon from "@mui/icons-material/AttachMoneyOutlined";
 import BoltOutlinedIcon from "@mui/icons-material/BoltOutlined";
-import AccountBalanceOutlinedIcon from "@mui/icons-material/AccountBalanceOutlined";
 import millify from "millify";
 import OneKPlusIcon from "@mui/icons-material/OneKPlus";
 import FormControl from "@mui/material/FormControl";
@@ -23,201 +19,352 @@ import {
 import {
   Paper,
   Box,
-  CircularProgress,
   Table,
   TableBody,
   TableContainer,
   TableRow,
   Typography,
   TableCell,
+  Container,
+  useMediaQuery,
 } from "@mui/material";
+import HTMLReactParser from "html-react-parser";
+import { humanize } from "../../helpers/humanize";
+import { useTheme } from "@mui/system";
+import CardData from "../../helpers/CardData";
+
+const calculatePeriod = (period) => {
+  const date = new Date();
+  const currentDate = date.getTime();
+  const newDate = date.setDate(date.getDate() - parseInt(period));
+  const currentDateMinusPeriod = new Date(newDate).getTime();
+  return [
+    Math.trunc(currentDateMinusPeriod / 1000),
+    Math.trunc(currentDate / 1000),
+  ];
+};
+
 const CryptocurrencyDetail = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
   const { coinId } = useParams();
-  const [period, setPeriod] = useState("7d");
+  const [period, setPeriod] = useState("7");
+  const currentDate = new Date();
+  let previousDate = currentDate.setDate(currentDate.getDate() - 1);
+  previousDate = Math.trunc(previousDate / 1000);
+  const [fromDate, setFromDate] = useState(previousDate);
+  const [toDate, setToDate] = useState(Math.trunc(new Date().getTime() / 1000));
   const { data, isFetching } = useGetCryptocurrencyDetailsQuery(coinId);
   const { data: coinHistory } = useGetCryptocurrencyHistoryQuery({
     coinId,
-    period,
+    fromDate,
+    toDate,
   });
-  if (isFetching)
-    return (
-      <Box sx={{ display: "flex" }}>
-        <CircularProgress color="secondary" />
-      </Box>
-    );
-  const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y", "5y"];
+  useEffect(() => {
+    const [fromDate, toDate] = calculatePeriod(period);
+    setFromDate(fromDate);
+    setToDate(toDate);
+  }, [period, coinId]);
+
+  const time = [
+    {
+      value: "24h",
+      period: "1",
+    },
+    {
+      value: "7d",
+      period: "7",
+    },
+    {
+      value: "30d",
+      period: "30",
+    },
+
+    {
+      value: "3m",
+      period: "90",
+    },
+    {
+      value: "6m",
+      period: "180",
+    },
+    {
+      value: "9m",
+      period: "270",
+    },
+    {
+      value: "1y",
+      period: "365",
+    },
+
+    {
+      value: "3y",
+      period: "1095",
+    },
+    {
+      value: "5y",
+      period: "1825",
+    },
+  ];
   const stats = [
     {
       index: 0,
       title: "Price to USD",
-      value: `$ ${data?.data?.coin?.price && millify(data?.data?.coin?.price)}`,
+      value: `$ ${
+        data?.market_data?.current_price?.usd &&
+        millify(data?.market_data?.current_price?.usd)
+      }`,
       icon: <AttachMoneyOutlinedIcon />,
     },
     {
       index: 1,
       title: "Rank",
-      value: data?.data?.coin?.rank,
+      value: data?.market_cap_rank,
       icon: <OneKPlusIcon />,
     },
     {
       index: 2,
-      title: "24h Volume",
-      value: `$ ${data?.data?.coin?.volume && millify(data?.data?.coin?.volume)
-        }`,
+      title: "Total Volume",
+      value: `$ ${
+        data?.market_data?.total_volume.usd &&
+        millify(data?.market_data?.total_volume.usd)
+      }`,
       icon: <BoltOutlinedIcon />,
     },
     {
       index: 3,
       title: "Market Cap",
-      value: `$ ${data?.data?.coin?.marketCap && millify(data?.data?.coin?.marketCap)
-        }`,
+      value: `$ ${
+        data?.market_data?.market_cap.usd &&
+        millify(data?.market_data?.market_cap.usd)
+      }`,
       icon: <AttachMoneyOutlinedIcon />,
     },
     {
       index: 4,
-      title: "All-time-high(daily avg.)",
-      value: `$ ${millify(data?.data?.coin?.allTimeHigh?.price)}`,
+      title: "Highest (daily avg.)",
+      value: `$ ${
+        data?.market_data?.ath?.usd ? millify(data?.market_data?.ath?.usd) : 0
+      }`,
       icon: <CardGiftcardIcon />,
     },
   ];
   const genericStats = [
     {
-      title: "Number Of Markets",
-      value: data?.data?.coin?.numberOfMarkets,
-      icon: <AccountBalanceOutlinedIcon />,
+      title: "Circulating Supply",
+      value: data?.market_data?.circulating_supply
+        ? data?.market_data?.circulating_supply
+        : 0,
+      icon: <PriorityHighOutlinedIcon />,
     },
     {
-      title: "Number Of Exchanges",
-      value: data?.data?.coin?.numberOfExchanges,
+      title: "Max Supply",
+      value: data?.market_data?.max_supply ? data?.market_data?.max_supply : 0,
       icon: <MoneyOutlinedIcon />,
     },
-    {
-      title: "Aprroved Supply",
-      value: data?.data?.coin?.approvedSupply ? (
-        <CheckOutlinedIcon />
-      ) : (
-        <PanToolOutlinedIcon />
-      ),
-      icon: <PriorityHighOutlinedIcon />,
-    },
+
     {
       title: "Total Supply",
-      value: `$ ${millify(data?.data?.coin?.totalSupply)}`,
-      icon: <PriorityHighOutlinedIcon />,
-    },
-    {
-      title: "Circulating Supply",
-      value: `$ ${millify(data?.data?.coin?.circulatingSupply)}`,
+      value: `$ ${
+        data?.market_data?.total_supply
+          ? millify(data?.market_data?.total_supply)
+          : 0
+      }`,
       icon: <PriorityHighOutlinedIcon />,
     },
   ];
-
-  return (
-    <Grid className="coin-detail-container">
-      <Grid item className="coin-heading-container">
-        <Typography className="coin-name">
-          {data?.data?.coin?.name} ({data?.data?.coin?.slug}) Price
-        </Typography>
-        <p>
-          {data?.data?.coin?.name} live price in US Dollar (USD). View value
-          statistics, market cap and supply.
-        </p>
-      </Grid>
-      <FormControl sx={{ m: 1, minWidth: 120, maxWidth: 450 }}>
-        <Select
-          labelId="time-chart-input"
-          id="time-select"
-          value={"7d"}
-          label="period"
-          onChange={(event) => setPeriod(event.target.value)}
+  const filteredLinks = [
+    "homepage",
+    "blockchain_site",
+    "official_forum_url",
+    "subreddit_url",
+  ];
+  return isFetching ? (
+    <div>Fetching</div>
+  ) : (
+    <Container maxWidth={matches ? "lg" : "sm"}>
+      <Box sx={{ marginTop: "2rem", marginBottom: "2rem" }}>
+        <Typography
+          sx={{ fontWeight: 900, fontSize: "1.5rem", color: "#12203F" }}
         >
-          {time.map((value, index) => (
-            <MenuItem value={value}>{value}</MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <Chart
-        coinHistory={coinHistory}
-        currentPrice={millify(data?.data?.coin?.price)}
-        coinName={data?.data?.coin?.name}
-      />
-
-      <Grid
+          {data?.name} ({data?.symbol}) Price{" "}
+        </Typography>
+        <Typography variant="body2" sx={{ p: 1 }}>
+          {data?.name} live price in US Dollar (USD). View value statistics,
+          market cap and supply.{" "}
+        </Typography>
+      </Box>
+      <Box sx={{ marginTop: "2rem", marginBottom: "2rem" }}>
+        <FormControl sx={{ m: 1, minWidth: 100, maxWidth: 450 }}>
+          <Select
+            labelId="time-chart-input"
+            id="time-select"
+            value={"7d"}
+            label="period"
+            name="time-chart-input"
+            onChange={(event) => setPeriod(event.target.value)}
+          >
+            {time.map((timeObject, index) => {
+              return (
+                <MenuItem value={timeObject.value} key={timeObject.period}>
+                  {timeObject.period} ({timeObject.value})
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+      </Box>
+      <Box sx={{ marginTop: "2rem", marginBottom: "2rem" }}>
+        <Chart
+          coinHistory={coinHistory}
+          currentPrice={
+            data?.market_data?.current_price?.usd
+              ? millify(data?.market_data?.current_price?.usd)
+              : 0
+          }
+          coinName={data?.name}
+        />
+      </Box>
+      <Box
+        sx={{ marginTop: "2rem", marginBottom: "2rem" }}
+        id="HERE"
         className="stats-container"
-        sx={{ display: "grid", gap: 3, gridTemplateColumns: "repeat(2, 1fr)" }}
       >
-        <Box className="coin-value-statistics">
-          <Grid className="coin-value-statistics-heading">
-            <Typography
-              className="coin-details-heading"
-              sx={{ fontSize: 27, fontWeight: "bold" }}
-            >
-              {data?.data?.coin?.name} Value Statistics
-            </Typography>
-            <p sx={{ margin: "15px 10px", padding: 5 }}>
-              An overview showing the statistics of {data?.data?.coin?.name},
-              such as the base and quote currency, the rank, and trading volume.
-            </p>
-          </Grid>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: "100%" }}>
-              <TableBody>
-                {stats.map(({ icon, title, value, index }) => (
-                  <TableRow key={index}>
-                    <TableCell sx={{ fontSize: 16, mt: 1 }}>{title}</TableCell>
-                    <TableCell
-                      sx={{
-                        fontWeight: "bold",
-                        color: "#4C65FB",
-                        fontSize: 22,
-                      }}
-                    >
-                      {value}
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+        <Box
+          className="coin-value-statistics-heading"
+          sx={{ marginTop: "2rem", marginBottom: "2rem" }}
+        >
+          <Typography
+            className="coin-details-heading"
+            sx={{ fontSize: "1.5rem", fontWeight: "bold" }}
+          >
+            {data?.name} Statistics
+          </Typography>
+          <Typography
+            variant="body2"
+            sx={{
+              margin: "15px 10px",
+            }}
+          >
+            An overview showing the statistics of {data?.name}, such as the base
+            and quote currency, the rank, and trading volume.
+          </Typography>
+          {CardData(stats)}
         </Box>
-        <Box className="other-stats-info">
-          <Grid className="coin-value-statistics-heading">
-            <Typography
-              className="coin-details-heading"
-              sx={{ fontSize: 27, fontWeight: "bold" }}
-            >
-              Other Stats Info
-            </Typography>
-            <p sx={{ margin: "15px 10px", padding: 5 }}>
-              An overview showing the statistics of {data?.data?.coin?.name},
-              such as the base and quote currency, the rank, and trading volume.
-            </p>
-          </Grid>
 
-          <TableContainer component={Paper}>
+        <Box
+          className="other-stats-info"
+          sx={{ marginTop: "2rem", marginBottom: "2rem" }}
+        >
+          <Typography
+            className="coin-details-heading"
+            sx={{ fontSize: "1.5rem", fontWeight: "bold" }}
+          >
+            Other Stats Info
+          </Typography>
+          <Typography
+            variant="body2"
+            sx={{
+              margin: "15px 10px",
+            }}
+          >
+            An overview showing the statistics of {data?.name}, such as the max
+            and total supply of the coin.
+          </Typography>
+
+          {CardData(genericStats, "rgba(252,161,71,1)")}
+        </Box>
+        <Box sx={{ marginTop: "2rem", marginBottom: "2rem" }}>
+          <Typography sx={{ fontSize: "1.5rem", fontWeight: "bold" }}>
+            What is {data?.name}
+          </Typography>
+          <Typography
+            variant="body2"
+            sx={{
+              margin: "15px 10px",
+            }}
+          >
+            {HTMLReactParser(data?.description?.en)}
+          </Typography>
+        </Box>
+        <Box sx={{ marginTop: "2rem", marginBottom: "2rem" }}>
+          <Typography sx={{ fontSize: "1.5rem", fontWeight: "bold" }}>
+            {data?.name} Links
+          </Typography>
+          <TableContainer
+            component={Paper}
+            sx={{
+              margin: "15px 10px",
+            }}
+          >
             <Table sx={{ minWidth: "100%" }}>
               <TableBody sx={{ minWidth: "100%" }}>
-                {genericStats.map(({ icon, title, value, index }) => (
-                  <TableRow sx={{ minWidth: "100%" }} key={index}>
-                    <TableCell sx={{ fontSize: 16, mt: 1 }}>{title}</TableCell>
-                    <TableCell
-                      sx={{
-                        fontWeight: "bold",
-                        color: "#4C65FB",
-                        fontSize: 22,
-                      }}
-                    >
-                      {value}
-                    </TableCell>
-                  </TableRow>
-                ))}
+                {Object.keys(data?.links).map((key) => {
+                  if (filteredLinks.includes(key)) {
+                    return (
+                      <TableRow sx={{ minWidth: "100%" }} key={key}>
+                        <TableCell sx={{ fontSize: 16, mt: 1 }}>
+                          {humanize(key)}
+                        </TableCell>
+                        <TableCell
+                          sx={{
+                            fontWeight: "bold",
+                            color: "#4C65FB",
+                            fontSize: 22,
+                          }}
+                        >
+                          {Array.isArray(data?.links[key]) ? (
+                            data?.links[key].map(
+                              (value) =>
+                                value && (
+                                  <>
+                                    <Link
+                                      to={{
+                                        pathname: value,
+                                      }}
+                                      style={{
+                                        fontWeight: "bold",
+                                        color: "#4C65FB",
+                                        fontSize: 16,
+                                      }}
+                                      target="_blank"
+                                      rel="noreferrer"
+                                    >
+                                      {value}
+                                    </Link>
+                                    <br />
+                                  </>
+                                )
+                            )
+                          ) : (
+                            <Link
+                              to={{
+                                pathname: data?.links[key],
+                              }}
+                              style={{
+                                fontWeight: "bold",
+                                color: "#4C65FB",
+                                fontSize: 16,
+                              }}
+                              target="_blank"
+                              rel="noreferrer"
+                            >
+                              {data?.links[key] || "Unknown"}
+                            </Link>
+                          )}
+                        </TableCell>
+                      </TableRow>
+                    );
+                  }
+                  return false;
+                })}
               </TableBody>
             </Table>
           </TableContainer>
         </Box>
-      </Grid>
-    </Grid>
+      </Box>
+    </Container>
   );
 };
 
-// export default CryptocurrencyDetail;
+export default CryptocurrencyDetail;
--- src/services/cryptocurrencyApi.js
+++ src/services/cryptocurrencyApi.js
@@ -14,27 +14,36 @@ export const cryptocurrencyApi = createApi({
       query: ({ rowsPerPage, pageNumber, sparkLine }) =>
         sparkLine
           ? makeRequest(
-            `/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${rowsPerPage}&page=${pageNumber + 1
-            }&sparkline=${sparkLine}&price_change_percentage=24h%2C7d`
-          )
-          : makeRequest(`/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${rowsPerPage}&page=${pageNumber + 1
+              `/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${rowsPerPage}&page=${
+                pageNumber + 1
+              }&sparkline=${sparkLine}&price_change_percentage=24h%2C7d`
+            )
+          : makeRequest(`/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${rowsPerPage}&page=${
+              pageNumber + 1
             }&sparkline=${sparkLine}
         `),
     }),
     getCryptocurrencyDetails: build.query({
-      query: (coinId) => makeRequest(`/coin/${coinId}`),
+      query: (coinId = "bitcoin") => makeRequest(`/coins/${coinId}`),
     }),
     getCryptocurrencyHistory: build.query({
-      query: ({ coinId, period }) =>
-        makeRequest(`coin/${coinId}/history/${period}`),
+      query: ({ coinId, fromDate, toDate }) =>
+        makeRequest(
+          `coins/${coinId}/market_chart/range?vs_currency=usd&from=${fromDate}&to=${toDate}`
+        ),
     }),
+
+    // /bitcoin/market_chart/range?vs_currency=usd&from=1392577232&to=1422577232
     getExchanges: build.query({
       query: ({ perPage = 10, pageNumber }) =>
         makeRequest(`/exchanges?per_page=${perPage}&page=${pageNumber + 1}`),
     }),
     getConvertRate: build.query({
-      query: ({ from = 'BTC', to = 'ETH' }) => makeRequest(`https://min-api.cryptocompare.com/data/price?fsym=${from}&tsyms=${to}`)
-    })
+      query: ({ from = "BTC", to = "ETH" }) =>
+        makeRequest(
+          `https://min-api.cryptocompare.com/data/price?fsym=${from}&tsyms=${to}`
+        ),
+    }),
   }),
 });
 
@@ -43,5 +52,5 @@ export const {
   useGetCryptocurrencyDetailsQuery,
   useGetCryptocurrencyHistoryQuery,
   useGetExchangesQuery,
-  useGetConvertRateQuery
+  useGetConvertRateQuery,
 } = cryptocurrencyApi;

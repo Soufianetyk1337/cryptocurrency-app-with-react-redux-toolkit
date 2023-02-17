893e768b0af1aa091227b86d48fdd5669173edeb
Soufianetyk1337 <sz.fanel@gmail.com>
2023-02-18 00:55:32 +0100

    Rendering a short news format in landing page

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,8 +1,9 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import { useGetCryptocurrenciesNewsQuery } from "../../services/cryptocurrencyNewsApi";
 import cryptoNewsAltImage from "../../static/images/cryptoNewsAltImage.jpg";
 import {
+  CircularProgress,
   Box,
   CardMedia,
   CardActions,
@@ -14,7 +15,6 @@ import {
   MenuItem,
   FormControl,
   Select,
-  Skeleton,
 } from "@mui/material";
 import moment from "moment";
 import { useGetCryptocurrenciesQuery } from "../../services/cryptocurrencyApi";
@@ -36,8 +36,24 @@ const News = ({ limit }) => {
     height: "100%",
     paddingTop: 2,
   };
-
-  return (
+  const newsDataSize = limit ? 6 : cryptocurrenciesNews?.results?.length;
+  const cryptocurrenciesNewsShort = cryptocurrenciesNews?.results.slice(
+    0,
+    newsDataSize
+  );
+  return isFetching ? (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <CircularProgress color="secondary" />
+    </Box>
+  ) : (
     <section className="section news" id="news">
       <Box
         sx={{
@@ -102,248 +118,155 @@ const News = ({ limit }) => {
           </FormControl>
         )}
         <Grid container spacing={2}>
-          {isFetching &&
-            [...Array(limit ? 6 : 12)].map((_, index) => (
-              <Grid item key={index} xs={12} sm={6} md={4} lg={4} xl={4}>
-                <article key={index}>
-                  <Box
-                    className="image__container"
-                    sx={{
-                      height: "200px",
-                      width: "100%",
-                    }}
-                  >
-                    <Skeleton
-                      sx={{ height: 200, width: 320 }}
-                      animation="wave"
-                      variant="rectangular"
-                    />
-                  </Box>
-
-                  {/* Card Body */}
-                  <Box
-                    className="post__body"
-                    sx={{
-                      padding: "1.1em",
-                      display: "flex",
-                      flexDirection: "column",
-                      flex: "1 1 auto",
-                      wordBreak: "break-all",
-                    }}
-                  >
-                    <Skeleton animation="wave" height={42} width="95%" />
-
-                    <Box
-                      className="post__card__content"
-                      sx={{
-                        flex: "1 1 auto",
-                      }}
-                    >
-                      <Skeleton animation="wave" height={66} width="95%" />
-                    </Box>
-                    <Box
-                      className="post__card__footer"
-                      sx={{
-                        flex: "1 1 auto",
-                        display: "flex",
-                      }}
-                    >
-                      <Box
-                        sx={{
-                          display: "flex",
-                        }}
-                      >
-                        <CardActions
-                          sx={{
-                            display: "flex",
-                            my: "10px",
-                            mx: 0,
-                            justifyContent: "space-between",
-                            padding: "5px",
-                          }}
-                        >
-                          <Skeleton
-                            animation="wave"
-                            variant="circular"
-                            width={48}
-                            height={48}
-                          />
-
-                          <Box sx={{ ml: 2 }}>
-                            <Fragment>
-                              <Skeleton
-                                animation="wave"
-                                height={21}
-                                style={{ marginBottom: 6 }}
-                              />
-                              <Skeleton
-                                animation="wave"
-                                height={21}
-                                width="80%"
-                              />
-                            </Fragment>
-                          </Box>
-                        </CardActions>
-                      </Box>
-                    </Box>
-                  </Box>
-                  {/* End of Card Body */}
-                </article>
-              </Grid>
-            ))}
-          {cryptocurrenciesNews &&
-            cryptocurrenciesNews?.value?.map((news, index) => (
-              <Grid item key={index} xs={12} sm={6} md={4} lg={4} xl={4}>
-                <article
-                  key={index}
-                  className="post__card"
-                  style={{
-                    display: "flex",
-                    height: "100%",
-                    flexDirection: "column",
-                    wordWrap: "break-word",
-                    position: "relative",
+          {cryptocurrenciesNewsShort.map((news, index) => (
+            <Grid item key={index} xs={12} sm={6} md={4} lg={4} xl={4}>
+              <article
+                key={index}
+                className="post__card"
+                style={{
+                  display: "flex",
+                  height: "100%",
+                  flexDirection: "column",
+                  wordWrap: "break-word",
+                  position: "relative",
+                  width: "100%",
+                }}
+              >
+                <Box
+                  className="image__container"
+                  sx={{
+                    height: "200px",
                     width: "100%",
                   }}
                 >
-                  <Box
-                    className="image__container"
-                    sx={{
-                      height: "200px",
-                      width: "100%",
-                    }}
-                  >
-                    <CardMedia
-                      component="img"
-                      image={
-                        (news?.image?.thumbnail?.contentUrl &&
-                          `${news?.image?.thumbnail?.contentUrl}
-                  &w=${news?.image?.thumbnail?.width}
-                   &h=${news?.image?.thumbnail?.height}`) ||
-                        cryptoNewsAltImage
-                      }
-                      alt={news?.name}
-                      style={{
-                        objectFit: "cover",
-                        verticalAlign: "center",
-                        maxWidth: "100%",
-                        width: "100%",
-                        height: "100%",
-                        imageRendering: "crisp-edges",
-                      }}
-                    />
-                  </Box>
-                  <Link
-                    href={news.url}
-                    target="_blank"
-                    rel="noreferrer"
-                    sx={{
-                      position: "absolute",
+                  <CardMedia
+                    component="img"
+                    image={
+                      (news?.image_url &&
+                        `${news?.image_url}
+                  `) ||
+                      cryptoNewsAltImage
+                    }
+                    alt={news?.title}
+                    style={{
+                      objectFit: "cover",
+                      verticalAlign: "center",
+                      maxWidth: "100%",
                       width: "100%",
                       height: "100%",
-                      top: 0,
-                      left: 0,
-                      ":hover": {
-                        border: "1px solid #8B84FE",
-                      },
+                      imageRendering: "crisp-edges",
                     }}
-                  ></Link>
-                  <Box
-                    className="post__body"
+                  />
+                </Box>
+                <Link
+                  href={news?.link}
+                  target="_blank"
+                  rel="noreferrer"
+                  sx={{
+                    position: "absolute",
+                    width: "100%",
+                    height: "100%",
+                    top: 0,
+                    left: 0,
+                    ":hover": {
+                      border: "1px solid #8B84FE",
+                    },
+                  }}
+                ></Link>
+                <Box
+                  className="post__body"
+                  sx={{
+                    padding: "1.1em",
+                    display: "flex",
+                    flexDirection: "column",
+                    flex: "1 1 auto",
+                    wordBreak: "break-all",
+                  }}
+                >
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="h5"
                     sx={{
-                      padding: "1.1em",
-                      display: "flex",
-                      flexDirection: "column",
+                      fontSize: "1rem",
+                      marginBottom: "1rem",
+                    }}
+                  >
+                    {`${news?.title?.substring(0, 60)}...`}
+                  </Typography>
+                  <Box
+                    className="post__card__content"
+                    sx={{
                       flex: "1 1 auto",
-                      wordBreak: "break-all",
                     }}
                   >
                     <Typography
                       gutterBottom
-                      variant="h5"
-                      component="h5"
+                      variant="p"
+                      component="p"
                       sx={{
-                        fontSize: "1rem",
-                        marginBottom: "1rem",
+                        fontSize: "0.875rem",
+                        lineHeight: "21px",
+                        color: "rgb(151, 160, 181)",
                       }}
                     >
-                      {`${news?.name?.substring(0, 60)}...`}
+                      {`${news?.description?.substring(0, 100)}...`}
                     </Typography>
+                  </Box>
+                  <Box
+                    className="post__card__footer"
+                    sx={{
+                      flex: "1 1 auto",
+                      display: "flex",
+                    }}
+                  >
                     <Box
-                      className="post__card__content"
                       sx={{
-                        flex: "1 1 auto",
-                      }}
-                    >
-                      <Typography
-                        gutterBottom
-                        variant="p"
-                        component="p"
-                        sx={{
-                          fontSize: "0.875rem",
-                          lineHeight: "21px",
-                          color: "rgb(151, 160, 181)",
-                        }}
-                      >
-                        {`${news?.description?.substring(0, 100)}...`}
-                      </Typography>
-                    </Box>
-                    <Box
-                      className="post__card__footer"
-                      sx={{
-                        flex: "1 1 auto",
                         display: "flex",
                       }}
                     >
-                      <Box
+                      <CardActions
                         sx={{
                           display: "flex",
+                          my: "10px",
+                          mx: 0,
+                          justifyContent: "space-between",
+                          padding: "5px",
                         }}
                       >
-                        <CardActions
-                          sx={{
-                            display: "flex",
-                            my: "10px",
-                            mx: 0,
-                            justifyContent: "space-between",
-                            padding: "5px",
-                          }}
-                        >
-                          <Avatar
-                            width="48"
-                            height="48"
-                            src={news.provider[0]?.image?.thumbnail?.contentUrl}
-                            sx={{ maxWidth: 48, maxHeight: 48 }}
-                            alt={news.provider[0]?.name}
-                            aria-label={`News Provided By${news.provider[0]?.name}`}
-                          />
+                        <Avatar
+                          maxWidth="48"
+                          maxheight="48"
+                          src={cryptoNewsAltImage}
+                          sx={{ maxWidth: 48, maxHeight: 48 }}
+                          alt={news.source_id}
+                          aria-label={`News Provided By${news?.source_id}`}
+                        />
 
-                          <Box sx={{ ml: 2 }}>
-                            <Typography
-                              gutterBottom
-                              variant="subtitle2"
-                              component="p"
-                            >
-                              {news.provider[0]?.name}
-                            </Typography>
-                            <Typography
-                              gutterBottom
-                              variant="subtitle2"
-                              component="p"
-                              color="textSecondary"
-                            >
-                              {moment(news?.datePublished)
-                                .startOf("ss")
-                                .fromNow()}
-                            </Typography>
-                          </Box>
-                        </CardActions>
-                      </Box>
+                        <Box sx={{ ml: 2 }}>
+                          <Typography
+                            gutterBottom
+                            variant="subtitle2"
+                            component="p"
+                          >
+                            {news.source_id}
+                          </Typography>
+                          <Typography
+                            gutterBottom
+                            variant="subtitle2"
+                            component="p"
+                            color="textSecondary"
+                          >
+                            {moment(news?.pubDate).startOf("ss").fromNow()}
+                          </Typography>
+                        </Box>
+                      </CardActions>
                     </Box>
                   </Box>
-                </article>
-              </Grid>
-            ))}
+                </Box>
+              </article>
+            </Grid>
+          ))}
         </Grid>
       </Box>
     </section>
